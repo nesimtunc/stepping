d4c323e87abf3218bbb425fdaa5b5a7c828ecf24
Nesim Tunc <nesimtunc@gmail.com>
2020-09-23 09:40:58 +0900

    Initial Commit

---
+++ Stepping/SteppingApp.swift
@@ -0,0 +1,20 @@
+//
+//  SteppingApp.swift
+//  Stepping
+//
+//  Created by Neso on 2020/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SteppingApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
