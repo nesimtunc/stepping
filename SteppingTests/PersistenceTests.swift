1707d8f89e48c385eaba507f94b51b850941b72c
Nesim Tunc <nesimtunc@gmail.com>
2020-09-30 21:23:30 +0900

    persistence tests

---
+++ SteppingTests/PersistenceTests.swift
@@ -0,0 +1,81 @@
+//
+//  PersistenseTests.swift
+//  SteppingTests
+//
+//  Created by Neso on 2020/09/30.
+//
+
+@testable import Stepping
+import XCTest
+import CoreData
+
+class PersistenceTests: XCTestCase {
+
+	var context: NSManagedObjectContext!
+
+	var validUUIDStr = "77048D46-2AB4-44B2-9C72-2764B8A899C5"
+
+	override func setUp() {
+		super.setUp()
+		self.context = PersistenceController.shared.container.viewContext;
+	}
+
+	override func tearDown() {
+		self.context = nil
+		super.tearDown()
+	}
+
+	private func createDummyItem() {
+		let newItem = BeaconItem(context: self.context)
+		newItem.id = UUID(uuidString: validUUIDStr)
+		newItem.name = "Test Beacon"
+		newItem.major = 3
+		newItem.minor = 1
+		newItem.title = "Did you forget something from your room?"
+		newItem.message = "Wallet, Pasmo"
+		newItem.timestamp = Date()
+		newItem.notifyOnEnter = false
+		newItem.notifyOnExit = true
+	}
+
+	private func getItemById(id: String) -> NSManagedObject {
+		let request = NSFetchRequest<NSFetchRequestResult>(entityName: "BeaconItem")
+		request.predicate = NSPredicate(format: "id == %@", validUUIDStr)
+
+		let result = try! self.context.fetch(request)
+		return (result.first as? NSManagedObject)!
+	}
+
+	func test_create_new_beacon_item() throws {
+		createDummyItem()
+		XCTAssertNoThrow(try self.context.save())
+	}
+
+	func test_update_existing_item() throws {
+		createDummyItem()
+
+		let item = getItemById(id: validUUIDStr)
+
+		item.setValue("Updated Beacon", forKey: "name")
+
+		XCTAssertNoThrow(try self.context.save())
+
+		let updatedItem = getItemById(id: validUUIDStr)
+
+
+		XCTAssertEqual((item.value(forKey: "name") as! String), (updatedItem.value(forKey: "name") as! String))
+	}
+
+	func test_delete_existing_item() throws {
+		createDummyItem()
+		let request = NSFetchRequest<NSFetchRequestResult>(entityName: "BeaconItem")
+		request.predicate = NSPredicate(format: "id == %@", validUUIDStr)
+
+		let result = try! self.context.fetch(request)
+		let item = result.first as? NSManagedObject
+
+		self.context.delete(item!)
+
+		XCTAssertNoThrow(try self.context.save())
+	}
+}
