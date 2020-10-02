9f31d08c54892cbbeb03ee9a1eaaee313ed54534
Nesim Tunc <nesimtunc@gmail.com>
2020-10-02 12:05:27 +0900

    UI tests for Adding new Beacon

--- SteppingUITests/SteppingUITests.swift
+++ SteppingUITests/SteppingUITests.swift
@@ -22,13 +22,41 @@ class SteppingUITests: XCTestCase {
 		// Put teardown code here. This method is called after the invocation of each test method in the class.
 	}
 
-	func testExample() throws {
+	func testAddNewBeacon() throws {
 		// UI tests must launch the application that they test.
 		let app = XCUIApplication()
+
 		app.launch()
 
-		// Use recording to get started writing UI tests.
-		// Use XCTAssert and related functions to verify your tests produce the correct results.
+		app.navigationBars["My iBeacons"].buttons["plus"].tap()
+
+		let tablesQuery = app.tables
+
+		let name = tablesQuery/*@START_MENU_TOKEN@*/.textFields["name"]/*[[".cells[\"Name ex: My Room\"]",".textFields[\"Name ex: My Room\"]",".textFields[\"name\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/
+		name.tap()
+		name.typeText("Test")
+
+		let uuid = tablesQuery.textFields["uuid"]
+		uuid.tap()
+		uuid.typeText("4AD3F9C4-EEA0-4067-93B9-30591748A143")
+
+		let major = tablesQuery.textFields["major"]
+		major.tap()
+		major.typeText("3")
+
+		let minor = tablesQuery.textFields["minor"]
+		minor.tap()
+		minor.typeText("1")
+
+		app.swipeUp()
+
+		let message = tablesQuery.textFields["message"]
+		message.tap()
+		message.typeText("Wallet, Keys?")
+
+		let save = tablesQuery.buttons["save"]
+		save.tap()
+
 	}
 
 	func testLaunchPerformance() throws {
